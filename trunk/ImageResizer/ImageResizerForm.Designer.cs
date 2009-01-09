ca70982a0ee0ae191eafed30ffa22e557678eaac
BriceLambson_cp <SND\BriceLambson_cp>
2009-01-09 09:00:56 -0800

    Missing files added.

---
+++ trunk/ImageResizer/ImageResizerForm.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace ImageResizer
+{
+	partial class ImageResizerForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImageResizerForm));
+			this.addButton = new System.Windows.Forms.Button();
+			this.removeButton = new System.Windows.Forms.Button();
+			this.imagesListView = new System.Windows.Forms.ListView();
+			this.browseButton = new System.Windows.Forms.Button();
+			this.outputLabel = new System.Windows.Forms.Label();
+			this.outputTextBox = new System.Windows.Forms.TextBox();
+			this.resizeButton = new System.Windows.Forms.Button();
+			this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+			this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+			this.SuspendLayout();
+			// 
+			// addButton
+			// 
+			this.addButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.addButton.Location = new System.Drawing.Point(12, 409);
+			this.addButton.Name = "addButton";
+			this.addButton.Size = new System.Drawing.Size(100, 23);
+			this.addButton.TabIndex = 4;
+			this.addButton.Text = "&Add Pictures...";
+			this.addButton.UseVisualStyleBackColor = true;
+			this.addButton.Click += new System.EventHandler(this.addButton_Click);
+			// 
+			// removeButton
+			// 
+			this.removeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.removeButton.Location = new System.Drawing.Point(118, 409);
+			this.removeButton.Name = "removeButton";
+			this.removeButton.Size = new System.Drawing.Size(150, 23);
+			this.removeButton.TabIndex = 5;
+			this.removeButton.Text = "R&emove Selected Pictures";
+			this.removeButton.UseVisualStyleBackColor = true;
+			this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+			// 
+			// imagesListView
+			// 
+			this.imagesListView.AllowDrop = true;
+			this.imagesListView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+						| System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.imagesListView.Location = new System.Drawing.Point(12, 41);
+			this.imagesListView.Name = "imagesListView";
+			this.imagesListView.Size = new System.Drawing.Size(600, 362);
+			this.imagesListView.Sorting = System.Windows.Forms.SortOrder.Ascending;
+			this.imagesListView.TabIndex = 3;
+			this.imagesListView.UseCompatibleStateImageBehavior = false;
+			this.imagesListView.DragDrop += new System.Windows.Forms.DragEventHandler(this.picturesListView_DragDrop);
+			this.imagesListView.DragEnter += new System.Windows.Forms.DragEventHandler(this.picturesListView_DragEnter);
+			this.imagesListView.KeyDown += new System.Windows.Forms.KeyEventHandler(this.imagesListView_KeyDown);
+			// 
+			// browseButton
+			// 
+			this.browseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.browseButton.Location = new System.Drawing.Point(537, 12);
+			this.browseButton.Name = "browseButton";
+			this.browseButton.Size = new System.Drawing.Size(75, 23);
+			this.browseButton.TabIndex = 2;
+			this.browseButton.Text = "&Browse...";
+			this.browseButton.UseVisualStyleBackColor = true;
+			this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+			// 
+			// outputLabel
+			// 
+			this.outputLabel.AutoSize = true;
+			this.outputLabel.Location = new System.Drawing.Point(12, 18);
+			this.outputLabel.Name = "outputLabel";
+			this.outputLabel.Size = new System.Drawing.Size(87, 13);
+			this.outputLabel.TabIndex = 0;
+			this.outputLabel.Text = "&Output Directory:";
+			// 
+			// outputTextBox
+			// 
+			this.outputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.outputTextBox.Location = new System.Drawing.Point(105, 14);
+			this.outputTextBox.Name = "outputTextBox";
+			this.outputTextBox.Size = new System.Drawing.Size(426, 20);
+			this.outputTextBox.TabIndex = 1;
+			// 
+			// resizeButton
+			// 
+			this.resizeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.resizeButton.Location = new System.Drawing.Point(487, 409);
+			this.resizeButton.Name = "resizeButton";
+			this.resizeButton.Size = new System.Drawing.Size(125, 23);
+			this.resizeButton.TabIndex = 6;
+			this.resizeButton.Text = "&Resize All Pictures...";
+			this.resizeButton.UseVisualStyleBackColor = true;
+			this.resizeButton.Click += new System.EventHandler(this.resizeButton_Click);
+			// 
+			// openFileDialog
+			// 
+			this.openFileDialog.Multiselect = true;
+			// 
+			// ImageResizerForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(624, 444);
+			this.Controls.Add(this.resizeButton);
+			this.Controls.Add(this.outputTextBox);
+			this.Controls.Add(this.outputLabel);
+			this.Controls.Add(this.browseButton);
+			this.Controls.Add(this.imagesListView);
+			this.Controls.Add(this.removeButton);
+			this.Controls.Add(this.addButton);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "ImageResizerForm";
+			this.Text = "Image Resizer";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button addButton;
+		private System.Windows.Forms.Button removeButton;
+		private System.Windows.Forms.ListView imagesListView;
+		private System.Windows.Forms.Button browseButton;
+		private System.Windows.Forms.Label outputLabel;
+		private System.Windows.Forms.TextBox outputTextBox;
+		private System.Windows.Forms.Button resizeButton;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+		private System.Windows.Forms.OpenFileDialog openFileDialog;
+	}
+}
+
