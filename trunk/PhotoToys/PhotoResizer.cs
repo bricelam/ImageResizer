0f6b927bedde765d62911ef445d7dbfe9c9196dd
BriceLambson_cp <SND\BriceLambson_cp>
2009-01-09 08:51:38 -0800

    Added licensing notices.

--- trunk/PhotoToys/PhotoResizer.cs
+++ trunk/PhotoToys/PhotoResizer.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿#region Common Public License Copyright Notice
+/**************************************************************************\
+* PhotoToys Clone                                                                   *
+*                                                                          *
+* Copyright © Brice Lambson. All rights reserved.                          *
+*                                                                          *
+* The use and distribution terms for this software are covered by the      *
+* Common Public License 1.0 (http://opensource.org/licenses/cpl1.0.php)    *
+* which can be found in the file CPL.txt at the root of this distribution. *
+* By using this software in any fashion, you are agreeing to be bound by   *
+* the terms of this license.                                               *
+*                                                                          *
+* You must not remove this notice, or any other, from this software.       *
+\**************************************************************************/
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows.Forms;
---
+++ trunk/README.txt
@@ -0,0 +1,10 @@
+PhotoToys Clone
+Copyright © Brice Lambson All rights reserved.
+Homepage: http://phototoysclone.sourceforge.net
+
+For licensing terms, see the adjacent LICENSE.txt file.
+
+
+In order to Debug, this project registers itself for COM interop and also writes
+keys and values to the regitry.  Because of this, Administrative privleges are
+required for debugging.
