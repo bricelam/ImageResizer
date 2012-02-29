8ff05a7bf4c39043612974e25cd3db294150191a
BriceLambson_cp <SND\BriceLambson_cp>
2012-02-28 19:38:48 -0800

    Fixing Work Item 7420 : Nothing happens when click on resize in latest version.

--- Main/Source/ShellExtensions/ContextMenuHandler.cpp
+++ Main/Source/ShellExtensions/ContextMenuHandler.cpp
@@ -136,7 +136,7 @@ HRESULT CContextMenuHandler::ResizePictures(CMINVOKECOMMANDINFO *pici)
 	CRegKey regKey;
 
 	// NOTE: The location is always read from a 32-bit key
-	if (!regKey.Open(HKEY_CURRENT_USER, _T("Software\\BriceLambson\\ImageResizer"), KEY_READ | KEY_WOW64_32KEY))
+	if (regKey.Open(HKEY_CURRENT_USER, _T("Software\\BriceLambson\\ImageResizer"), KEY_READ | KEY_WOW64_32KEY))
 	{		
 		regKey.Open(HKEY_LOCAL_MACHINE, _T("Software\\BriceLambson\\ImageResizer"), KEY_READ | KEY_WOW64_32KEY);
 	}
