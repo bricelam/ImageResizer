50faaab395aab0c0a0ae0a2e546f3f55a2e9f5dc
Brice Lambson <bricelam@outlook.com>
2016-09-07 21:31:05 -0700

    Build using Cake

--- build.cmd
+++ build.cmd
@@ -1,11 +1,1 @@
-@ECHO OFF
-
-IF NOT EXIST .nuget\nuget.exe (
-    MKDIR .nuget
-    powershell.exe -Command "& {iwr https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile .nuget\nuget.exe}"
-)
-
-IF NOT DEFINED VisualStudioVersion CALL "%VS140COMNTOOLS%VsDevCmd.bat"
-
-.nuget\nuget.exe restore
-MSBuild.exe ImageResizer.proj /m /v:m /nr:false /nologo %*
+@powershell.exe -File build.ps1 %*
